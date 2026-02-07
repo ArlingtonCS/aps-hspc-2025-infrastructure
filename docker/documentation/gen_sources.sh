57151268337ebce05dd0dd0a2616c05074f278b3
Brayden Zee <github@blueish.dev>
2026-02-07 01:30:45 -0500

    Add documentation

---
+++ docker/documentation/gen_sources.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# download this to this folder: https://download.oracle.com/otn-pub/java/jdk/17.0.18%2B8/31b2ae140a1c4f24b46d6848c2ac260f/jdk-17.0.18_doc-all.zip
+
+unzip jdk-17.0.18_doc-all.zip -d jdk-17.0.18_doc-all
+curl -OL https://docs.python.org/3/archives/python-3.14-docs-html.tar.bz2
+curl -OL https://github.com/PeterFeicht/cppreference-doc/releases/download/v20250209/html-book-20250209.tar.xz
